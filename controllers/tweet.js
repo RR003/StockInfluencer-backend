7809fd08b1994af86cbf750b6453e5c80441e064
RR003 <mail2rahulraja@gmail.com>
2022-03-05 22:03:57 -0500

    db sort changes

--- controllers/tweet.js
+++ controllers/tweet.js
@@ -19,3 +19,25 @@ export const createTweet = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+export const allTimeChange = async (req, res) => {
+  try {
+    let querySort = { LongTermChange: -1 };
+    let tweets = await Tweet.find().limit(100).sort(querySort);
+    res.status(201).json(tweets);
+  } catch (error) {
+    res.status(409).json({ message: "Something wrong, whoops..." });
+  }
+};
+
+export const allWeekChange = async (req, res) => {
+  try {
+    let timeSort = { Time: -1 };
+    let querySort = { ShortTermChange: -1 };
+    let unfiltered = await Tweet.find().sort(timeSort).limit(500);
+    let tweets = await unfiltered.sort(querySort);
+    res.status(201).json(tweets);
+  } catch (error) {
+    res.status(409).json({ message: "Something wrong, whoops..." });
+  }
+};
--- routes/dbroutes.js
+++ routes/dbroutes.js
@@ -1,7 +1,14 @@
 import express from "express";
-import { createTweet } from "../controllers/tweet.js";
+import {
+  createTweet,
+  allTimeChange,
+  allWeekChange,
+} from "../controllers/tweet.js";
 
 const router = express.Router();
 
 router.post("/creatingTweet", createTweet);
+
+router.get("/getAllTimeChange", allTimeChange);
+router.get("/getWeekChange", allWeekChange);
 export default router;
